ef5dfe3ae99586de366c214cdce355b024c1c13f
Lex Rivera <me@lex.io>
2014-12-07 03:01:18 +0300

    [php_npc] added

---
+++ manifests/php_npc.pp
@@ -0,0 +1,18 @@
+define newrelic_plugin_agent::php_npc (
+    $scheme = 'http',
+    $host = 'localhost',
+    $verify_ssl_cert = 'true',
+    $port = '80',
+    $path = '/apc-nrp.php',
+  ) {
+  include newrelic_plugin_agent::params
+
+  $newrelic_plugin_agent_conffile = $newrelic_plugin_agent::params::newrelic_plugin_agent_conffile
+
+  concat::fragment { "newrelic_plugin_agent-php_npc-${name}":
+    order   => '13',
+    target  => $newrelic_plugin_agent_conffile,
+    content => template('newrelic_plugin_agent/php_npc.erb'),
+  }
+
+}
